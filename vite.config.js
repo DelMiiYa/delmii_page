42993852fdfbc132495d33d728df4778d7774c3c
DelMiiYa <siriyaaaaa36@gmail.com>
2026-07-14 12:37:16 +0700

    fix: correct base path in Vite configuration for consistency

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/delmii_page/",
+  base: "/delmii-page/",
   plugins: [react(), tailwindcss()],
 });
